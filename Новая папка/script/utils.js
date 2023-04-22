f20c8d34066876a2366dacf5aa34e7e0d4e14ea1
VALERII-A <saligasaliga100@gmail.com>
2023-04-22 20:24:43 +0500

    delete

--- Новая папка/script/utils.js
+++
@@ -1,6 +0,0 @@
-export function setDataRefresh(minutes, key) {
-    const setTime = new Date(new Date().getTime() + minutes * 600);
-    localStorage.setItem(key, setTime);
-    return setTime;
-  }
-  
